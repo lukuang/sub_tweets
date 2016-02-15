ea88086449f2eb7df5d05257861d14757f81c57d
lukuang <lukuang1989@gmail.com>
2016-02-15 12:04:00 -0500

    test grouping files in days

--- parse_json.py
+++ parse_json.py
@@ -43,15 +43,37 @@ def write_to_xml(dest_file,content):
     with open(dest_file,"w") as f:
         f.write(content)
 
+def get_daily_files(source_dir):
+    daily_files = {}
+    files = os.walk(source_dir).next()[2]
+    for f in files:
+        m = re.search("^(.+?)-\d+$",f)
+        if m is None:
+            print "file name error"
+            print f
+            sys.exit(-1)
+        else:
+            day = m.group(1)
+            if day not in daily_files:
+                daily_files[day] = []
+            daily_files.append(f)
+    return daily_files
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("json_file")
+    parser.add_argument("source_dir")
     parser.add_argument("--no_dup","--n",action="store_true")
     parser.add_argument("dest_dir")
     args=parser.parse_args()
-    content = parse_tweet_json(args.json_file,args.no_dup)
-    dest_file = os.path.join(args.dest_dir, os.path.basename(args.json_file))
-    write_to_xml(dest_file,content)
+    daily_files = get_daily_files(args.source_dir)
+    print daily_files
+    sys.exit(0)
+    for day in daily_files:
+        content = ""
+        for hour in daily_files[day]:
+            content += parse_tweet_json(hour,args.no_dup)
+        dest_file = os.path.join(args.dest_dir, day)
+        write_to_xml(dest_file,content)
 
 if __name__=="__main__":
     main()
